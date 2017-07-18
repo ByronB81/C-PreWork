20f8c39900168640e0d9616fa1d3990a9218f09f
Byron Blocker <byronblocker@gmail.com>
2017-07-18 15:56:25 -0700

    tried to add decryptor but my brain is broken

--- interactive/Cryptor.cs
+++ interactive/Cryptor.cs
@@ -4,6 +4,14 @@ class Cryptor
 {
     static void Main()
     {
+        var string phraseHolder;
+
+        Encrypt();
+        Decrypt(phraseHolder);
+    }
+
+    static void Encrypt()
+    {
         Console.WriteLine("Please enter a text you would like to encrypt");
         string userInput = Console.ReadLine();
         string aEncrypt = userInput.Replace("a", "4");
@@ -11,7 +19,20 @@ class Cryptor
         string iEncrypt = eEncrypt.Replace("i", "1");
         string oEncrypt = iEncrypt.Replace("o", "0");
 
-        string encryptedResult = oEncrypt.Replace("u", "V");
+        string encryptedResult = oEncrypt.Replace("u", "^");
+        var phraseHolder = encryptedResult;
         Console.WriteLine(encryptedResult);
+    }
+
+    static void Decrypt(string codedPhrase)
+    {
+        string aDecrypt = codedPhrase.Replace("4","a");
+        string eDecrypt = aDecrypt.Replace("3", "e");
+        string iDecrypt = eDecrypt.Replace("1", "i");
+        string oDecrypt = iDecrypt.Replace("0", "o");
+
+        string decryptedResult = oDecrypt.Replace("^", "v");
+        Console.WriteLine(decryptedResult);
+
     }
 }
