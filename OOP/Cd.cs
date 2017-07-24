275a4663843f682db6f31b3760c7162037b21710
Byron Blocker <byronblocker@gmail.com>
2017-07-24 14:31:34 -0700

    recursive fun

--- OOP/Cd.cs
+++ OOP/Cd.cs
@@ -6,28 +6,25 @@ class Cd
     public string Title;
     public string Artist;
     public int Price;
+
+    public Cd(string albumTitle, string albumArtist, int albumPrice = 10)
+    {
+        Title = albumTitle;
+        Artist = albumArtist;
+        Price = albumPrice;
+    }
 }
 
 public class Program
 {
     public static void Main()
     {
-        Cd first = new Cd();
-        first.Title = "Master of Reality";
-        first.Artist = "Black Sabbath:";
-        first.Price = 10;
+        Cd first = new Cd("Master of Reality", "Black Sabbath");
+        Cd second = new Cd("Electric Ladyland", "Jimi Hendrix");
+        Cd third = new Cd("Nevermind", "Nirvana");
+        Cd fourth = new Cd("I Don't Get It", "Pork Lion", 50);
 
-        Cd second = new Cd();
-        second.Title = "Electric Ladyland";
-        second.Artist = "Jimi Hendrix";
-        second.Price = 10;
-
-        Cd third = new Cd();
-        third.Title = "Nevermind";
-        third.Artist = "Nirvana";
-        third.Price = 10;
-
-        List<Cd> Cds= new List<Cd>() {first, second, third};
+        List<Cd> Cds= new List<Cd>() {first, second, third, fourth};
 
         foreach (Cd album in Cds)
         {
