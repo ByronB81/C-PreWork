c9607e8614b57b978bfd51f67f7eeca8b207d5f1
Byron Blocker <byronblocker@gmail.com>
2017-07-24 14:06:04 -0700

    add Adoptable method

--- OOP/Animal.cs
+++ OOP/Animal.cs
@@ -6,10 +6,23 @@ class Animal
     public string Name;
     public string Breed;
     public int Age;
+    public bool Adopted;
 
     public bool IsACat()
     {
         if (Breed.Contains("Cat"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Adoptable()
+    {
+        if (Adopted == false)
         {
             return true;
         }
@@ -28,21 +41,34 @@ public class Program
         pet1.Name = "Toby";
         pet1.Breed = "Golden Retriever";
         pet1.Age = 4;
+        pet1.Adopted = false;
 
         Animal pet2 = new Animal();
         pet2.Name = "Boots";
         pet2.Breed = "Calico Cat";
         pet2.Age = 6;
+        pet2.Adopted = false;
 
         Animal pet3 = new Animal();
         pet3.Name = "Harley";
         pet3.Breed = "Labrador Retriever";
         pet3.Age = 2;
+        pet3.Adopted = false;
 
         List<Animal> petList = new List<Animal>() {pet1, pet2, pet3};
 
         pet3.Name = "Harley the Great!";
+        Console.WriteLine("Here are all of our pets: ");
 
+        foreach (Animal pet in petList)
+        {
+            if (pet.Adoptable())
+            {
+                Console.WriteLine(pet.Name);
+            }
+        }
+
+        Console.WriteLine("This pet best fits your search: ");
         foreach (Animal pet in petList)
         {
             if (pet.Age > 3 && !pet.IsACat())
@@ -50,5 +76,6 @@ public class Program
                 Console.WriteLine(pet.Name + " the " + pet.Breed);
             }
         }
+
     }
 }
